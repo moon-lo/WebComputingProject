db9eaa5dfaa33b6713aea602b8c26b9e80a7de61
Moon <moonxdd@gmail.com>
2016-04-13 13:03:24 +1000

    Added template and script.js

---
+++ HTML/scripts.js
@@ -0,0 +1,52 @@
+function validate () {
+	
+	var validated = true;
+	
+	if (checkName()) {
+		//window.alert("Missing surname");
+		document.getElementById("surnameMissing").style.visibility = "visible";
+		document.getElementById("surname").focus();
+		validated = false;
+	}
+	
+	if (checkPassword()) {
+		window.alert("Please make sure that the passwords match");
+		validated = false;
+	}
+	
+	if (validated) {
+		window.alert("Form validated!");
+	}
+	
+	return validated;
+	
+}
+
+function checkName() {
+	var x = document.forms["myform"]["surname"].value;
+	if (x == null || x == "") {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+function hideError() {
+	document.getElementById("surnameMissing").style.visibility = "hidden";
+}
+
+function checkPassword() {
+	var p = document.forms["myform"]["psw"].value;
+	var p2 = document.forms["myform"]["conpsw"].value;
+	
+	if (p == null || p == "" || p2 == null || p2 == "") {
+		return true;
+	}
+	else if (p  !== p2) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
